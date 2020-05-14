ac502957496c7a5b150c704e6fa85de95dd21bbf
MatheusHAS <mathkeka@gmail.com>
2020-05-14 17:25:10 -0300

    update themeColor to purple[800]

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primaryColor: Colors.pinkAccent,
+        primaryColor: Colors.purple[800],
         iconTheme: IconThemeData(color: Colors.black),
       ),
       home: ChatScreen(),
